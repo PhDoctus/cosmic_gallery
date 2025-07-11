e1d968c03a09c7e14f882f15f0f0967e15490f85
PHDoctus <132488648+PhDoctus@users.noreply.github.com>
2025-07-11 15:30:43 -0400

    Add files via upload

---
+++ javascript_cosmic_gallery.js
@@ -0,0 +1,7 @@
+// List of gallery images
+const galleryImages = [
+    'imgs_cosmic_gallery/hubble-ngc2005-potw2424a.jpg',
+    'imgs_cosmic_gallery/hubble_terzan2_potw2228a.jpg',
+    'imgs_cosmic_gallery/ngc4654-1-v5f-final.jpg',
+    'imgs_cosmic_gallery/stsci-h-p2042a-f-1663x1663-1.png'
+];
